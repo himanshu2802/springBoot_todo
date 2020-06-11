8dadda69d27c2762576f534e46b9e10bdba4387f
himanshu2802 <shrivastava.r.himanshu@gmail.com>
2020-06-11 16:55:58 +0530

    Changed for Jenkins

--- src/main/java/com/in28minutes/rest/webservices/jwt/auth/SecurityConfigurer.java
+++ src/main/java/com/in28minutes/rest/webservices/jwt/auth/SecurityConfigurer.java
@@ -47,6 +47,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 		return NoOpPasswordEncoder.getInstance();
 	}
 	
+	
 	@Bean
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
