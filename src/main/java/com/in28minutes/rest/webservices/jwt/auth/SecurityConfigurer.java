93135d087d02d62c23587fcfd0b78216777565b0
himanshu2802 <shrivastava.r.himanshu@gmail.com>
2020-06-11 16:43:48 +0530

    testee

--- src/main/java/com/in28minutes/rest/webservices/jwt/auth/SecurityConfigurer.java
+++ src/main/java/com/in28minutes/rest/webservices/jwt/auth/SecurityConfigurer.java
@@ -46,7 +46,6 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 		return NoOpPasswordEncoder.getInstance();
 	}
 	
-	
 	@Bean
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
