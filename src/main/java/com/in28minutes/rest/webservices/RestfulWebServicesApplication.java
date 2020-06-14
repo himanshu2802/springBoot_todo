b5d4a8e680872315a791205989717d64f7737c74
himanshu2802 <shrivastava.r.himanshu@gmail.com>
2020-06-14 15:46:27 +0530

    extended class in main

--- src/main/java/com/in28minutes/rest/webservices/RestfulWebServicesApplication.java
+++ src/main/java/com/in28minutes/rest/webservices/RestfulWebServicesApplication.java
@@ -2,12 +2,19 @@ package com.in28minutes.rest.webservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestfulWebServicesApplication {
+public class RestfulWebServicesApplication extends SpringBootServletInitializer	 {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
+	
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(RestfulWebServicesApplication.class);
+	  }
 
 }
